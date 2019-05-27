d5fc398fbc4b2349d9afc8abacc135b452c0317d
Ethan Vernon <ethan.vernon@colorado.edu>
2019-05-27 08:16:22 +0800

    Updates

---
+++ precache-manifest.2ff151b99943f750d707547fe3c85c06.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2365d1619b8d53737e64c114e4313762",
+    "url": "/random-test-questions/index.html"
+  },
+  {
+    "revision": "0dcabe66fd4c6a1d5b47",
+    "url": "/random-test-questions/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "eb5d03ae7c9a30028659",
+    "url": "/random-test-questions/static/js/2.4e81ba5b.chunk.js"
+  },
+  {
+    "revision": "0dcabe66fd4c6a1d5b47",
+    "url": "/random-test-questions/static/js/main.9264660b.chunk.js"
+  },
+  {
+    "revision": "96502e7b6f2c5114eabf",
+    "url": "/random-test-questions/static/js/runtime~main.35396c46.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/random-test-questions/static/media/logo.5d5d9eef.svg"
+  }
+]);
